8281fa1dbce2efb3d820376c8b44368aa88ef9cf
Cássia Oliveira dos Santos <cassia.oliveira.profissional@gmail.com>
2024-10-10 00:43:54 -0300

    feat: arquivo de conexão com o banco de dados

---
+++ db.php
@@ -0,0 +1,15 @@
+
+$servername = "localhost"; // Define o nome do servidor de banco de dados, geralmente "localhost".
+$username = "root"; // Nome de usuário para acessar o banco de dados.
+$password = ""; // Senha para acessar o banco de dados.
+$dbname = "erpl"; // Nome do banco de dados que você deseja acessar.
+
+// Cria uma conexão com o banco de dados usando as credenciais fornecidas.
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verifica se a conexão foi bem-sucedida.
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error); // Se houver erro, a execução para e uma mensagem é exibida.
+}
+
+?>
